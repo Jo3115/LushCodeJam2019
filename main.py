3de951652795fba192c84c9c6bd85bd49163f2e4
Jo3115 <49487746+Jo3115@users.noreply.github.com>
2019-06-12 13:04:04 +0100

    updating webpage

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     """Displays The Homepage"""
-    return render_template('testing.html',
-                           test=2)
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
