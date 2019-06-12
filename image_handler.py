c06f86347c81bea4c8a74c15666b33655d92dd82
Jo3115 <49487746+Jo3115@users.noreply.github.com>
2019-06-12 12:32:40 +0100

    made image handler

---
+++ image_handler.py
@@ -0,0 +1,27 @@
+import requests
+import pprint
+from PIL import Image
+from io import BytesIO
+
+
+def get_image_from_name():
+    params={
+        'key':'AIzaSyBKV4ru8MuUJmvo3JRf-XhzcwYyBf6bx8M',
+        'cx':'015680093206351405511:notwdjcaeve',
+        'q':'lush bath bombs Harajuku'}
+    data = requests.get('https://www.googleapis.com/customsearch/v1?',
+                        params).json()
+    image_url = data['items'][0]['pagemap']['product'][0]['image']
+    print(image_url)
+    return image_url
+
+
+def get_image_colour(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    colors_values = img.getcolors(256)
+    pprint(colors_values)
+
+
+if __name__ == '__main__':
+    get_image_colour(get_image_from_name())
